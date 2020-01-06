ee3786706366b0c1a9539bf67f148c8f2f3de95f
puskini33 <elenahirjoaba@yahoo.com>
2020-01-06 15:27:27 +0100

    add string_scanner files

---
+++ string_scanner/scanner_string_segment.py
@@ -0,0 +1,10 @@
+class ScanedStringSegment(object):
+
+    def __init__(self, token, index, start, end):
+        self.token = token
+        self.index = index
+        self.start_string = start
+        self.end_string = end
+
+    def __repr__(self):
+        return f'MatchedStringSegment({self.token}, {self.index}, {self.start_string}, {self.end_string})'
