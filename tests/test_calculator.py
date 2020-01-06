efcc754c316862c4a118d5d710838160ed87847a
puskini33 <elenahirjoaba@yahoo.com>
2020-01-06 21:57:08 +0100

    update tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,21 @@
+from world_state import WorldState
+from StringParser import Parser
+from StringAnalyzer import Analyzer
+from StringInterpreter import Interpreter
+from regex_tokens.regex_rules import RegexRules
+from string_scanner.scanner import Scanner
 import unittest
-from runner import *
+
 
 
 class TestCalculator(unittest.TestCase):
 
     def test_calculator(self):
-        test_scanner = SetupScanner(TOKENS, code)
+        code = ["1 + 2 ="]
+        regex_rules = RegexRules()
+        test_scanner = Scanner(regex_rules.list_regex_rules, code)
 
-        test_parser = SetupParser(test_scanner.scanned_tree)
+        test_parser = Parser(test_scanner)
         test_parsed_string = test_parser.parse()
         print(test_parsed_string)
 
@@ -20,14 +28,6 @@ class TestCalculator(unittest.TestCase):
         test_interpreted_tree = test_interpreter.interpret(test_world_state)
 
 
-code = ["1 + 2 ="]
-
-TOKENS = [
-            ((r"^[0-9]+"),                 "INTEGER"),
-            ((r"^\+"),                     "PLUS"),
-            ((r"\s"),                      "SPACE"),
-            ((r"\="),                      "EQUAL"),
-            ((r"[a-z]"),                   "VARIABLE")]
 
 
 if __name__ == "__main__":
--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,25 +1,27 @@
+from string_scanner.scanner import Scanner
+from string_scanner.scanner_string_segment import ScannedStringSegment
+from regex_tokens.enum_tokens import TokenName
+from regex_tokens.regex_rules import RegexRules
 import unittest
-from StringScanner import Scanner
 
 
 class TestScanner(unittest.TestCase):
 
+    def test_scan(self):
+        regex_rules = RegexRules()
+        local_token_name = TokenName.integer.value
+        local_scanner = Scanner(regex_rules.list_regex_rules, '1')
+        self.assertEqual(local_scanner.list_tokens, [ScannedStringSegment(local_token_name, 0, 1, 1)])
+        # self.assertRaises(AssertionError, lambda: Scanner(regex_rules.list_regex_rules, code))  # TODO: Ask what is the role of testing for errors. Is it to catch the errors in advance to deal with them afterwards in the code?
+
     def test_match(self):
+        regex_rules = RegexRules()
         code = ["1 + 2 ="]
-
-        TOKENS = [
-                    ((r"^[0-9]+"),                 "INTEGER"),
-                    ((r"^\+"),                     "PLUS"),
-                    ((r"\s"),                      "SPACE"),
-                    ((r"\="),                      "EQUAL"),
-                    ((r"[a-z]"),                   "VARIABLE")]
-
-        test_local_scanner = Scanner(TOKENS, code)
-
+        test_local_scanner = Scanner(regex_rules.list_regex_rules, code)
         self.assertEqual(test_local_scanner.match('INTEGER'), ['INTEGER', '1'])
         self.assertEqual(test_local_scanner.peek(), 'PLUS')
-        self.assertEqual(test_local_scanner.skip('PLUS'), True)
-        self.assertEqual(test_local_scanner.push(('PLUS', '+')), None)
+        # self.assertEqual(test_local_scanner.skip('PLUS'), True)
+        # self.assertEqual(test_local_scanner.push(('PLUS', '+')), None)
 
 
 if __name__ == "__main__":
